11150bf829e5b1962c361e5bb3ddaafadde3808c
Kbenjamin92 <kipplbenjamin@gmail.com>
2022-05-24 17:33:55 -0400

    working on the handleChange function

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Header";
+import SearchForm from "./components/SearchForm";
 
 const App = () => {
+  const [userInput, setUserInput] = useState<string>("");
+
+  // add the proper type for the handleChange function parameter
+  const handleChange = (e: React.FormEvent<HTMLElement>) => {
+    
+    console.log(e);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+    console.log('form submited');
+  }
+
 
   return (
-    <div className="App">
-      <Header />
-      <Main />
+    <div className='App'>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <SearchForm 
+          input={userInput} 
+          handleChange={handleChange} 
+          handleSubmit={handleSubmit}
+          />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,34 +1,47 @@
 import React from "react";
-import '../styles/SearchForm.css'
-import Fetch from './Fetch';
-import Form from 'react-bootstrap/Form'
-import { 
-  mainContent,
-  subText,
-  searchButton 
-} from '../content'
+import "../styles/SearchForm.css";
+import Fetch from "./Fetch";
+import Form from "react-bootstrap/Form";
+import { mainContent, subText, searchButton } from "../content";
 
+type UserInput = {
+  input: string;
+  handleChange: (e: React.ReactEventHandler) => void;
+  handleSubmit: (e: React.FormEvent<HTMLElement>) => void;
+};
 
-const SearchForm = () => {
+const SearchForm: React.FC<UserInput> = (props) => {
+  const { 
+    input, 
+    handleChange, 
+    handleSubmit } = props;
 
   return (
-    <div className="container">
-      <Form className="form-container">
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label className="search-label">{mainContent}</Form.Label>
-          <div className="search-operation">
-            <Form.Control type="text" className="search-input-field" placeholder="Search Scriptures..." />
-            <button className="search-btn">{searchButton}</button>
+    <div className='container' onSubmit={handleSubmit}>
+      <Form className='form-container'>
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <Form.Label className='search-label'>{mainContent}</Form.Label>
+          <div className='search-operation'>
+            <Form.Control
+              name='input'
+              type='text'
+              className='search-input-field'
+              placeholder='Search Scriptures...'
+              onChange={handleChange}
+            />
+            <button className='search-btn'>{searchButton}</button>
           </div>
-          <div className="sub-text">
-            <Form.Text className="text-muted">
-              {subText}
-            </Form.Text>
+          <div className='sub-text'>
+            <Form.Text className='text-muted'>{subText}</Form.Text>
           </div>
         </Form.Group>
       </Form>
+
+      <p>{input}</p>
+
       <Fetch />
     </div>
-)};
+  );
+};
 
 export default SearchForm;
