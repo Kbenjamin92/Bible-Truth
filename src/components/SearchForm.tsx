74791d0d112677e256f1f738d270859c68794482
Kbenjamin92 <kipplbenjamin@gmail.com>
2022-05-24 15:58:22 -0400

    styling updates

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import '../styles/SearchForm.css'
 import Fetch from './Fetch';
 import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
 import { 
   mainContent,
   subText,
@@ -11,22 +10,21 @@ import {
 
 
 const SearchForm = () => {
+
   return (
     <div className="container">
       <Form className="form-container">
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>{mainContent}</Form.Label>
+          <Form.Label className="search-label">{mainContent}</Form.Label>
           <div className="search-operation">
             <Form.Control type="text" className="search-input-field" placeholder="Search Scriptures..." />
-            <Button variant="success">{searchButton}</Button>
+            <button className="search-btn">{searchButton}</button>
           </div>
-          
           <div className="sub-text">
             <Form.Text className="text-muted">
               {subText}
             </Form.Text>
           </div>
-          
         </Form.Group>
       </Form>
       <Fetch />
